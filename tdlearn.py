455c16abd44633a766bc3e0dded114bd15384801
Parley Belsey <kevinbelsey@PBelsey.verizon.net>
2018-12-04 10:11:41 -0500

    syntax

--- tdlearn.py
+++ tdlearn.py
@@ -1,4 +1,3 @@
-
 from __future__ import division 
 import numpy as np 
 from numpy import array
@@ -11,9 +10,9 @@ import string
 import matplotlib.pyplot as plt
 
 def update_Qi(Qval0, Qval1, reward1, alpha, gamma, beta):
-    """ update q-value of selected action, given reward and alpha
-    """
-    return Qval0 + alpha * (reward1 + gamma*Qval1 - Qval0)
+	""" update q-value of selected action, given reward and alpha
+	"""
+	return Qval0 + alpha * (reward1 + gamma*Qval1 - Qval0)
 
 class Player(object):
 	# defines other players in BeerGame 
@@ -30,7 +29,7 @@ class Player(object):
 	def fill_orders(self, orders): 
 		if self.inventory >= orders: 
 			self.inventory -= orders 
-		else if self.inventory = 0 
+		elif self.inventory == 0: 
 			self.backorders += orders
 		else: 
 			self.backorders = orders - self.inventory 
@@ -78,107 +77,105 @@ class BeerGame(object):
 
 # adapted from qlearning.py in ADMCode 
 class TDagent(object):
-    """ defines the learning parameters of single td-learning-learning agent
-    in a 
-    ::Arguments::
-        alpha (float): learning rate
-        beta (float): inverse temperature parameter
-        gamma (float): discount rate 
-        preward (list): 1xN vector of reward probaiblities for each of N bandits
-        rvalues (list): 1xN vector of payout values for each of N bandits
-                        IF rvalues is None, all values set to 1
-    """
-    def __init__(self, alpha=.04, beta=3.5, gamma=.02, epsilon=.1, lowbound=0, 
-    	highbound=10, inventory=4, demand=4):
-        if (inventory<0 or demand<=0 or lowbound<0 or highbound<lowbound):
-            raise Exception('parameters make no sense')
-        self.beer = BeerGame(lowbound=lowbound, highbound=highbound, inventory=inventory, demand=demand)
-        self.updateQ = lambda Qval, r, alpha: Qval + alpha*(r - Qval)
-        self.updateP = lambda Qvector, act_i, beta: np.exp(beta*Qvector[act_i])/np.sum(np.exp(beta*Qvector))
-        self.set_params(alpha=alpha, beta=beta, gamma=gamma, epsilon=epsilon)
+	""" defines the learning parameters of single td-learning-learning agent
+	in a 
+	::Arguments::
+		alpha (float): learning rate
+		beta (float): inverse temperature parameter
+		gamma (float): discount rate 
+		preward (list): 1xN vector of reward probaiblities for each of N bandits
+		rvalues (list): 1xN vector of payout values for each of N bandits
+						IF rvalues is None, all values set to 1
+	"""
+	def __init__(self, alpha=.04, beta=3.5, gamma=.02, epsilon=.1, lowbound=0, highbound=10, inventory=4, demand=4):
+		if (inventory<0 or demand<=0 or lowbound<0 or highbound<lowbound):
+			raise Exception('parameters make no sense')
+		self.beer = BeerGame(lowbound=lowbound, highbound=highbound, inventory=inventory, demand=demand)
+		self.updateQ = lambda Qval, r, alpha: Qval + alpha*(r - Qval)
+		self.updateP = lambda Qvector, act_i, beta: np.exp(beta*Qvector[act_i])/np.sum(np.exp(beta*Qvector))
+		self.set_params(alpha=alpha, beta=beta, gamma=gamma, epsilon=epsilon)
 
-    def set_params(self, **kwargs):
-        """ update learning rate, inv. temperature, and/or
-        epsilon parameters of q-learning agent
-        """
+	def set_params(self, **kwargs):
+		""" update learning rate, inv. temperature, and/or
+		epsilon parameters of q-learning agent
+		"""
+		kw_keys = list(kwargs)
 
-        kw_keys = list(kwargs)
+		if 'alpha' in kw_keys:
+			self.alpha = kwargs['alpha']
 
-        if 'alpha' in kw_keys:
-            self.alpha = kwargs['alpha']
+		if 'beta' in kw_keys:
+			self.beta = kwargs['beta']
 
-        if 'beta' in kw_keys:
-            self.beta = kwargs['beta']
+		if 'gamma' in kw_keys: 
+			self.gamma = kwargs['gamma']
 
-        if 'gamma' in kw_keys: 
-        	self.gamma = kwargs['gamma']
+		if 'epsilon' in kw_keys:
+			self.epsilon = kwargs['epsilon']
 
-        if 'epsilon' in kw_keys:
-            self.epsilon = kwargs['epsilon']
-
-        self.nact = self.highbound-self.lowbound
-        self.actions = np.arange(self.nact)
+		self.nact = self.highbound-self.lowbound
+		self.actions = np.arange(self.nact)
 
 	def play_beergame(self, ntrials=1000, get_output=True):
-	    """ simulates agent performance on a beer game task 
-	    ::Arguments::
-	        ntrials (int): number of trials to play bandits
-	        get_output (bool): returns output DF if True (default)
-	    ::Returns::
-	        DataFrame (Ntrials x Nbandits) with trialwise Q and P
-	        values for each bandit
-	    """
-	    pdata = np.zeros((ntrials+1, self.nact))
-	    pdata[0, :] = np.array([1/self.nact]*self.nact)
-	    qdata = np.zeros_like(pdata)
-	    self.choices = []
-	    self.feedback = []
+		""" simulates agent performance on a beer game task 
+		::Arguments::
+			ntrials (int): number of trials to play bandits
+			get_output (bool): returns output DF if True (default)
+		::Returns::
+			DataFrame (Ntrials x Nbandits) with trialwise Q and P
+			values for each bandit
+		"""
+		pdata = np.zeros((ntrials+1, self.nact))
+		pdata[0, :] = np.array([1/self.nact]*self.nact)
+		qdata = np.zeros_like(pdata)
+		self.choices = []
+		self.feedback = []
 
-	    for t in range(ntrials):
+		for t in range(ntrials):
+			# select bandit arm (action) from state space 
+			act_i = np.random.choice(self.actions, p=pdata[t, :])
 
-	        # select bandit arm (action) from state space 
-	        act_i = np.random.choice(self.actions, p=pdata[t, :])
+			# get reward for current action  
+			r = self.bandits.get_reward(act_i)
 
-	        # get reward for current action  
-	        r = self.bandits.get_reward(act_i)
+			for i in range(t): 
+				last = self.choices[i]
 
-	        for i in range(t): 
-	        	last = self.choices[i]
+				# update value of selected action
+				qdata[i, last] = update_Qi(qdata[i, last], qdata[i+1, act_j], self.alpha, self.gamma)
 
-	        	# update value of selected action
-	        	qdata[i, last] = update_Qi(qdata[i, last], qdata[i+1, act_j], self.alpha, self.gamma)
+			# broadcast old q-values for unchosen actions
+			for act_j in self.actions[np.where(self.actions!=act_i)]:
+				qdata[t+1, act_j] = qdata[t, act_j]
 
-	        # broadcast old q-values for unchosen actions
-	        for act_j in self.actions[np.where(self.actions!=act_i)]:
-	            qdata[t+1, act_j] = qdata[t, act_j]
+			# update action selection probabilities and store data
+			pdata[t+1, :] = update_Pall(qdata[t+1, :], self.beta)
+			self.choices.append(act_i)
+			self.feedback.append(r)
 
-	        # update action selection probabilities and store data
-	        pdata[t+1, :] = update_Pall(qdata[t+1, :], self.beta)
-	        self.choices.append(act_i)
-	        self.feedback.append(r)
+		self.pdata = pdata[1:, :]
+		self.qdata = qdata[1:, :]
+		self.make_output_df()
 
-	    self.pdata = pdata[1:, :]
-	    self.qdata = qdata[1:, :]
-	    self.make_output_df()
+		if get_output:
+			return self.data.copy()
 
-	    if get_output:
-	        return self.data.copy()
-
-	    def make_output_df(self):
-        """ generate output dataframe with trialwise Q and P measures for each bandit,
-        as well as choice selection, and feedback
-        """
-	        df = pd.concat([pd.DataFrame(dat) for dat in [self.qdata, self.pdata]], axis=1)
-	        columns = np.hstack(([['{}{}'.format(x, c) for c in self.actions] for x in ['q', 'p']]))
-	        df.columns = columns
-	        df.insert(0, 'trial', np.arange(1, df.shape[0]+1))
-	        df['choice'] = self.choices
-	        df['feedback'] = self.feedback
-	        r = np.array(self.bandits.rvalues)
-	        p = np.array(self.bandits.preward)
-	        df['optimal'] = np.where(df['choice']==np.argmax(p * r), 1, 0)
-	        df.insert(0, 'agent', 1)
-	        self.data = df.copy()
+		def make_output_df(self):
+			""" 
+			generate output dataframe with trialwise Q and P measures for each
+			bandit, as well as choice selection, and feedback 
+			"""
+			df = pd.concat([pd.DataFrame(dat) for dat in [self.qdata, self.pdata]], axis=1)
+			columns = np.hstack(([['{}{}'.format(x, c) for c in self.actions] for x in ['q', 'p']]))
+			df.columns = columns
+			df.insert(0, 'trial', np.arange(1, df.shape[0]+1))
+			df['choice'] = self.choices
+			df['feedback'] = self.feedback
+			r = np.array(self.bandits.rvalues)
+			p = np.array(self.bandits.preward)
+			df['optimal'] = np.where(df['choice']==np.argmax(p * r), 1, 0)
+			df.insert(0, 'agent', 1)
+			self.data = df.copy()
 
 
 
